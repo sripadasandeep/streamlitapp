8089e44f602a27ce32169bbf9ab22f3c4df721f2
sripadasandeep <149664958+sripadasandeep@users.noreply.github.com>
2023-11-02 14:17:01 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,116 @@
+import streamlit as st
+from PIL import Image
+import io
+import base64
+import cv2
+import requests
+import json
+
+def testingfunc(base64_string):
+    # base64_string = 
+    # Convert the Base64 string to binary data
+    binary_data = base64.b64decode(base64_string)
+    url = 'http://164.52.201.54:18080/verify_liveness'
+    headers = {
+        "Content-Type": "image/jpeg",  
+    }
+    r = requests.post(url,data=binary_data, headers=headers)
+    json_object = json.loads(r.text)
+    print(json_object)
+    return json_object
+
+
+# Set a title for the Streamlit app
+st.title("Image Capture and Upload App")
+
+# Create a sidebar selection for image source
+image_source = st.sidebar.selectbox("Select Image Source", ("Capture from Webcam", "Upload from Local"))
+
+if image_source == "Capture from Webcam":
+    st.subheader("Webcam Capture")
+    
+    # Use st.empty() to create a placeholder for the webcam image
+    webcam_image_placeholder = st.empty()
+    
+    # Use st.button() to trigger image capture
+    if st.button("Capture Image"):
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        ret, frame = cap.read()
+        cap.release()
+
+        # Save the captured image to a file
+        if ret:
+            cv2.imwrite("captured_image.jpg", frame)
+            st.write("Image captured and waiting for response...")
+        # Open and display the captured image
+        captured_image = Image.open("captured_image.jpg")
+        webcam_image_placeholder.image(captured_image, caption="Captured Image", use_column_width=True)
+        # Open the image file
+        with open("captured_image.jpg", "rb") as image_file:
+            # Read the binary data of the image
+            image_binary = image_file.read()
+
+        # Encode the binary data as base64
+        image_base64 = base64.b64encode(image_binary).decode()
+
+        response = testingfunc(image_base64)
+        print(response)
+        st.write(response)
+
+        # if st.button("re capture"):
+        #     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        #     ret, frame = cap.read()
+        #     cap.release()
+
+        #     # Save the captured image to a file
+        #     if ret:
+        #         cv2.imwrite("captured_image.jpg", frame)
+        #         st.write("Image captured and saved as 'captured_image.jpg'")
+        #     # Open and display the captured image
+        #     captured_image = Image.open("captured_image.jpg")
+        #     webcam_image_placeholder.image(captured_image, caption="Captured Image", use_column_width=True)
+        # if st.button("test"):
+        #     # Open the image file
+        #     with open("captured_image.jpg", "rb") as image_file:
+        #         # Read the binary data of the image
+        #         image_binary = image_file.read()
+
+        #     # Encode the binary data as base64
+        #     image_base64 = base64.b64encode(image_binary).decode()
+
+        #     response = testingfunc(image_base64)
+        #     print(response)
+        #     st.write(response)
+        
+        # # You can use libraries like OpenCV to capture an image from the webcam
+        # # Replace the following code with your image capture logic
+        # # You should capture a single frame as an image
+        # captured_image = Image.open("path_to_captured_image.jpg")
+        # # Display the captured image
+        # webcam_image_placeholder.image(captured_image, caption="Captured Image", use_column_width=True)
+
+else:
+    # Upload an image from the local system
+    st.subheader("Upload Image")
+    
+    # Create a file uploader widget
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+    print(f"uploaded_image:{uploaded_image}")
+    if uploaded_image is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_image)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+        image_bytes = io.BytesIO()
+        image.save(image_bytes, format="JPEG")
+        image_bytes = image_bytes.getvalue()
+
+        # Encode bytes in base64
+        image_base64 = base64.b64encode(image_bytes).decode()
+
+        response = testingfunc(image_base64)
+        print(response)
+        st.write(response)
+    else:
+        pass   
+    
+# Run the Streamlit app
